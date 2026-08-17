597fefa9ddb1b42d60d21e9d0250ce268fff3a77
GuilhermeTavares13 <gui.tavares2002.contato@gmail.com>
2026-08-17 06:29:23 -0300

    finish my solution

--- expenses/expensesSlice.js
+++ expenses/expensesSlice.js
@@ -4,8 +4,7 @@ import * as Crypto from 'expo-crypto';
 export const expensesSlice = createSlice({
     name: 'expenses',
     initialState: {
-        expenses: [],
-        recentExpenses: []
+        expenses: []
     },
     reducers: {
         addExpenses: (state, action) => {
@@ -17,9 +16,38 @@ export const expensesSlice = createSlice({
             }
 
             state.expenses.push(aExpense);
+        },
+        editExpenses: (state, action) => {
+
+            let expenseIndex = -1;
+
+            state.expenses.forEach((expense, index) => {
+                if (expense.id == action.payload.id) {
+                    expenseIndex = index;
+                    return;
+                }
+            });
+
+            console.log(expenseIndex);
+
+            state.expenses[expenseIndex].name = action.payload.name,
+            state.expenses[expenseIndex].value = action.payload.value,
+            state.expenses[expenseIndex].date = action.payload.date
+        },
+        deleteExpense: (state, action) => {
+            let expenseIndex = -1;
+
+            state.expenses.forEach((expense, index) => {
+                if (expense.id == action.payload.id) {
+                    expenseIndex = index;
+                    return;
+                }
+            });
+
+            state.expenses.splice(expenseIndex, 1);
         }
     }
 });
 
-export const { addExpenses } = expensesSlice.actions;
+export const { addExpenses, editExpenses, deleteExpense  } = expensesSlice.actions;
 export default expensesSlice.reducer;
